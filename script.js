608e8e56ddd1aaa50a48583395ef91e1f545b615
R73939133 <103775733+R73939133@users.noreply.github.com>
2025-02-20 22:52:10 +0600

    Code corrected

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ const players = [
     {
         name: "Md Jim Arian",
         position: "Winger", number: 8,
-        image: "image/jim.jpg",
+        image: "image/Jim.jpg",
         stats: { goals: 8, assists: 10, matches: 16, minutesPlayed: 1450 }
     },
     {
